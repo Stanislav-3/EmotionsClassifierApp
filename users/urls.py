3d558933eec6deafbab92c33cd0e787d4173a3db
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-06-29 23:11:33 +0300

    add image to User model

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
+    path('profile', views.profile, name='profile'),
 ]
