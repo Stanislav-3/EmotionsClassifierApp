9c0e20f61fd4287a1690204cd1e1e7f17c3f4cb5
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-06-30 14:55:20 +0300

    add profile edit

--- users/models.py
+++ users/models.py
@@ -1,9 +1,6 @@
 import os
 from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, AbstractUser
-from django.contrib.auth import get_user_model
 
 
 def get_upload_path(instance, filename):
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView
+from .views import CustomPasswordChangeView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
     path('profile', views.profile, name='profile'),
+    path('password_change/',
+         CustomPasswordChangeView.as_view(template_name="registration/change_password.html"),
+         name='password_change'),
 ]
