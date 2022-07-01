6381ee67ff9e1083d1670749f29804e3c7f24321
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-07-01 13:40:01 +0300

    add computation & its page

--- computations/views.py
+++ computations/views.py
@@ -3,17 +3,43 @@ from django.contrib.auth import login, logout
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from .forms import ComputationForm
+from .models import Computation
+from computations.evaluation import predict
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+
+
+target_names = {
+    0: 'Angry',
+    1: 'Disgust',
+    2: 'Fear',
+    3: 'Happy',
+    4: 'Sad',
+    5: 'Surprise',
+    6: 'Neutral',
+}
 
 
 def computations(request):
-    if request.method == 'POST':
-        form = ComputationForm(request.POST)
-        if form.is_valid():
-            # compute
-            form.save()
-            # print result
-            # return redirect(reverse('home'))
-    else:
-        form = ComputationForm()
+    output = 'Results will be here'
 
-    return render(request, 'computations/computations.html', {'form': form})
+    if request.method == 'POST':
+        image = request.FILES['image'].file
+        # output = request.POST['output']
+        probabilities = predict(image)
+        output = '\n'
+        for idx in probabilities.argsort()[::-1]:
+            tabs = '\t\t' if len(target_names[idx]) > 3 else '\t\t\t'
+            output += f'{target_names[idx]} {tabs} {100 * probabilities[idx]:.1f}\n'
+
+        # add a new computation to db
+        file_content = ContentFile(request.FILES['image'].read())
+        computation = Computation(predictions=list(probabilities),
+                                  user=request.user)
+        computation.save()
+        computation.image.save(f'{computation.id}.jpg', file_content)
+    else:
+        pass
+
+    return render(request, 'computations/computations.html', {'output': output})
