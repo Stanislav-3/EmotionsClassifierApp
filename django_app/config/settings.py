b312274472c05ff477237a3efabb0840d922f0b4
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2023-04-17 13:34:40 +0300

    reconfigure smtp

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -158,11 +158,10 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'EmotionsClassifierAppBot2@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Thisisverysecure_forsure!'
-EMAIL_HOST_PASSWORD ='trdsjupvachitmnx'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
